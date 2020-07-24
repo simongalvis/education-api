d709165a820301d0c521052eab3b0636e4300fce
Simon Galvis <simongalvis9@gmail.com>
2020-07-24 13:25:56 -0400

    Get results of api search displayed in DOM

---
+++ index.js
@@ -0,0 +1,82 @@
+function submitform() {
+    $('#school-search-form').on('submit', function(event) {
+        event.preventDefalt();
+
+    })
+}
+'use strict';
+
+
+const apiKey = '2baff53cae2d4ab826a5355443a529ba';
+const appID = '095e8d22';
+const searchURL = 'https://api.schooldigger.com/v1.2/schools';
+var state = $();
+
+function formatQueryParams(params) {
+    const queryItems = Object.keys(params)
+        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    return queryItems.join('&');
+}
+
+function findSchools(query, state) {
+    const params = {
+        appKey: apiKey,
+        appID: appID,
+        q: query,
+        st: state,
+        perPage: 50
+
+    };
+    const queryString = formatQueryParams(params);
+    const url = searchURL + "?" + queryString;
+
+    console.log(url);
+
+    fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => displayResults(responseJson))
+        .catch(err => {
+            $("#js-error-message").text(`Something went wrong: ${err.message}`);
+        });
+}
+
+
+function displayResults(responseJson) {
+
+    console.log(responseJson);
+    $('#results-list').empty();
+    // iterate through the items array
+    for (let i = 0; i < responseJson.schoolList.length; i++) {
+
+
+        $("#results-list").append(
+            `<li>${responseJson.schoolList[i].schoolName} </li>`
+        );
+    }
+
+}
+
+
+
+function watchForm() {
+    $('form').submit(event => {
+        event.preventDefault();
+        const searchTerm = $('#js-search-term').val();
+
+        const searchState = $('#js-state').val();
+        findSchools(searchTerm, searchState);
+        console.log(searchState)
+        $(".insertState").text(`Search results for ${searchTerm}`);
+    });
+}
+
+$(watchForm);
+
+
+
+//Base url: https://api.schooldigger.com
